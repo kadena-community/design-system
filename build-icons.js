0d8a6bdfc201acdcd346fd261b8bacd238412974
Takadenoshi <tasos@kadena.io>
2024-02-08 17:36:02 +0100

    Autogenerate icons/README.md

--- build-icons.js
+++ build-icons.js
@@ -13,9 +13,26 @@ async function getFiles(dir) {
   return Array.prototype.concat(...files);
 }
 
+function makeIconsReadme(template, iconFilenames, templateFilename) {
+  const headerStopText = '## Icons';
+  let headerStopIdx = template.indexOf(headerStopText);
+  if (headerStopIdx === -1) {
+    throw new Error(`The template stop line "${headerStopText}" was not found in the Icons readme template: ${templateFilename}`);
+  }
+  let readmeContents = `${template.slice(0, headerStopIdx)}${headerStopText}\n\n`;
+  const iconSection = iconFilenames.map(iconFilename => {
+    const absoluteIconsPath = resolve(__dirname, 'icons');
+    const relativeFilename = relative(absoluteIconsPath, iconFilename);
+    let iconName = iconFilename.substr(iconFilename.lastIndexOf('/')+1);
+    iconName = iconName.replace('.svg', '').replace(/[_-]/g, ' ');
+    return `![${iconName}](https://raw.githubusercontent.com/kadena-community/design-system/main/icons/${relativeFilename} "${iconName}")`;
+  });
+  return readmeContents + iconSection.join('\n');
+}
+
 (async function () {
-  const icons = await getFiles('./icons');
-  const svgs = icons.filter(x => x.endsWith('.svg')).map(svg => {
+  const iconFilenames = (await getFiles('./icons')).filter(x => x.endsWith('.svg'));
+  const svgs = iconFilenames.map(svg => {
     const content = readFileSync(svg, 'utf8');
     const file = relative('./icons', svg).split('/').pop();
     const name = (content.match(/name="(.*?)"/) ?? [])[1] ?? file.replace('.svg', '');
@@ -48,7 +65,12 @@ async function getFiles(dir) {
     }
   }
 
-  const writeFile = join(__dirname, './tokens/foundation/icon/svg.tokens.json')
-  writeFileSync(writeFile, JSON.stringify(result, null, 2), 'utf8')
-})()
+  const jsonFilename = join(__dirname, './tokens/foundation/icon/svg.tokens.json');
+  writeFileSync(jsonFilename, JSON.stringify(result, null, 2), 'utf8');
 
+  const readmeTemplateFilename = join(__dirname, './icons/README.md.template');
+  const readmeFilename = join(__dirname, './icons/README.md');
+  const template = readFileSync(readmeTemplateFilename).toString();
+  const readmeContents = makeIconsReadme(template, iconFilenames, readmeTemplateFilename);
+  writeFileSync(readmeFilename, readmeContents);
+})()
