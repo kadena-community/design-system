88951cf05c22e96d958722deff069a1616ae4c0e
Isa Ozler <isaozler@gmail.com>
2024-06-10 16:44:41 +0200

    added build export tokens for direct import

--- build-tokens.js
+++ build-tokens.js
@@ -2,13 +2,16 @@
 const { resolve, join } = require('path');
 const { writeFile } = require('fs/promises');
 const { readFileSync } = require('fs');
-const { readdir } = require('fs').promises;
+const { readdir, rmdir, unlink } = require('fs').promises;
 const set = require('lodash/set')
 const merge = require('lodash/merge')
 const isObject = require('lodash/isObject');
 const { isArray } = require('lodash');
 
-const StyleDictionary = require('./build-style-dictionary-tokens')
+const StyleDictionary = require('./build-style-dictionary-tokens');
+
+const buildPath = './builds/tokens'
+const excludeExportPaths = [];
 
 async function getFiles(dir) {
   const dirents = await readdir(dir, { withFileTypes: true });
@@ -17,6 +20,25 @@ async function getFiles(dir) {
     return dirent.isDirectory() ? getFiles(res) : res;
   }));
   return Array.prototype.concat(...files);
+}
+
+async function removeDirectoryContents(directory) {
+  try {
+    const items = await readdir(directory, { withFileTypes: true });
+
+    for (const item of items) {
+      const itemPath = join(directory, item.name);
+
+      if (item.isDirectory()) {
+        await removeDirectoryContents(itemPath);
+        await rmdir(itemPath);
+      } else {
+        await unlink(itemPath);
+      }
+    }
+  } catch (error) {
+    console.error(`Error removing contents of ${directory}:`, error);
+  }
 }
 
 function getAllValues(obj) {
@@ -64,6 +86,32 @@ function getAllValues(obj) {
   return result;
 }
 
+async function createExportFiles() {
+  try {
+    const content = readFileSync(`${buildPath}/kda-design-system.tokens.json`, 'utf8');
+
+    await removeDirectoryContents(buildPath + '/export');
+
+    if (content) {
+      const rawData = JSON.parse(content).kda.foundation;
+
+      Object.keys(rawData).filter(foundationItemKey => !excludeExportPaths.includes(foundationItemKey)).forEach(async (foundationItemKey) => {
+        const foundationItemExportPath = join(__dirname, `${buildPath}/export/${foundationItemKey}.tokens.json`)
+        await writeFile(foundationItemExportPath, JSON.stringify({
+          kda: {
+            foundation: {
+              [foundationItemKey]: rawData[foundationItemKey]
+            },
+          },
+        }), { flag: 'w', encoding: 'utf-8' })
+      }, {})
+    }
+
+  } catch (error) {
+    console.error(`Error while exporting tokens.`, error)
+  }
+}
+
 (async function () {
   const tokens = await getFiles('./tokens');
 
@@ -83,9 +131,12 @@ function getAllValues(obj) {
     }
   }, {});
 
-  const destPath = './builds/tokens/kda-design-system.raw.tokens.json'
-  const writeFilePath = join(__dirname, destPath)
+  const destPath = `${buildPath}/kda-design-system.raw.tokens.json`;
+  const writeFilePath = join(__dirname, destPath);
   await writeFile(writeFilePath, JSON.stringify(result), { flag: 'w', encoding: 'utf-8' })
 
   StyleDictionary(destPath)
+
+  createExportFiles()
+
 })()
